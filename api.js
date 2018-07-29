3dcdd1383819d3d3719f555148d00ede68177dc4
AaronRobert <Robert.Aaron2017@outlook.com>
2018-07-29 19:46:54 +0800

    update

--- api.js
+++ api.js
@@ -6,8 +6,6 @@ const db = require("./database.js");
 const User = require("./user");
 const JsonGenerator = require("./json_generator/index.js");
 
-User.db = db;
-
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -15,8 +13,21 @@ app.get('/',(req,res)=>{
     res.send('APM Backend');
 });
 
-app.get('/api/user/profile/:name',function(req,res){
-    User.getUser(req.params.name,user=>res.send(user));
+app.get('/api/user/:name/profile',function(req,res){
+    var user = User.createNew();
+        
+    db.hmget(`users:${req.params.name}`,user.metadata.getDatabaseField).then(val=>{
+        if(res[0]===null)return res.send("null");
+        for(var i=0;i<val.length;i++){
+            user[user.metadata.getDatabaseField[i]] = val[i];
+        }
+        user.metadata = undefined;
+        res.send(user);
+    });
+});
+
+app.get('/api/user/:name/contribute',function(req,res){
+    
 });
 
 module.exports = app;
--- user.js
+++ user.js
@@ -1,19 +1,6 @@
 const Util = require("./util");
 
 var User = {
-    db: {},
-    getUser(name,callback){
-        var user = this.createNew();
-        
-        this.db.hmget(`users:${name}`,user.metadata.getDatabaseField).then(res=>{
-            if(res[0]===null)return callback("null");
-            for(var i=0;i<res.length;i++){
-                user[user.metadata.getDatabaseField[i]] = res[i];
-            }
-            user.metadata = undefined;
-            callback(user);
-        });
-    },
     createNew (){
         var user = {};
         var metadata = {};
@@ -32,5 +19,4 @@ var User = {
         return user;
     }
 };
-var user = User.createNew();
 module.exports = User;
