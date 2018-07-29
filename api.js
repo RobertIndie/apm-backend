98f1e0d06cc03fbcae5d79010a4ec35f5b126235
AaronRobert <Robert.Aaron2017@outlook.com>
2018-07-29 13:09:19 +0800

    add redis

--- api.js
+++ api.js
@@ -2,12 +2,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const joi = require('joi');
+const {promisify} = require("util");
+
+const JsonGenerator = require("./json_generator/index.js");
+
+var redis = require("redis"),
+    client = redis.createClient();
+
+const getAsync = promisify(client.get).bind(client);
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+client.on("ready",function (){
+    console.log("Redis connect successful");
+})
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/',(req,res)=>{
     res.send('APM Backend');
+});
+
+app.get('/api/user/profile/:id',function(req,res){
+    getAsync("test").then(val=>{
+        res.send(val);
+    });
 });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const app = require("./api.js");
-const JsonGenerator = require("./json_generator/index.js");
-
-projectList = [];
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => console.log(`Listening on port ${port} ...`));
